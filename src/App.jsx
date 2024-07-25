35dd8b824e7d9c7d6f55d0c348dadb28087ce580
unknown <DH@DH-CARRERA-34.DH.digitalhouse.com>
2024-07-24 22:08:55 -0300

    Clase 2: Creación de proyecto con Vite

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Componente from "./Components/Componente";
+
+function App() {
+  return (
+    <>
+      <h1>Bienvenidos a Vite</h1>
+      <Componente />
+      <Componente></Componente>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Components/Componente.jsx
@@ -0,0 +1,4 @@
+export default function Componente() {
+  let nombre = "Sammy";
+  return <h1>Hola, {nombre}! Este es mi primer componente</h1>;
+}
